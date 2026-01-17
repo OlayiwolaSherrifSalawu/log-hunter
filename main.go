58f05592388cbea8bce819d7d60b8210bacd88d8
Salawu Olayiwola Sherrif <salawuolayiwola181@gmail.com>
2026-01-17 23:29:47 +0100

    updated: CountError method

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Logstats struct {
 	filename   string
 	errorCount int
 }
 
-func (s *Logstats) countError(filename string)
+func (s *Logstats) CountError(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error while opening file:", err)
+		return
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		} else if strings.Contains(line, "ERROR") {
+			s.errorCount += 1
+		}
+	}
+	defer file.Close()
+	fmt.Println("they are " + strconv.Itoa(s.errorCount) + " in file")
+}
+func main() {
+	s := Logstats{}
+	s.CountError("server.log")
+}
